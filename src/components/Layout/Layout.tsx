fe32b9f9ee74b6ac74e1d9c0072a01876b5d0c4c
Ohm Piromrak <ohmpzzz@gmail.com>
2020-12-26 10:01:32 +0700

    Hello, The Space Post 🚀

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface LayoutProps {}
+
+export function Layout(props: LayoutProps) {
+    return <>This is Layout,, For Navbar and stuff</>
+}
+
+export default Layout
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+
+export function IndexPage(props: RouteComponentProps) {
+    return <>Hello, The Space Post</>
+}
+
+export default IndexPage
