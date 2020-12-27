c100e9fd81b20dde155fb4f8f5bb455255e7b2f1
chayanin rojarat <chayaninrojarat@gmail.com>
2020-12-27 23:15:49 +0700

    create nav

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,82 +1,33 @@
 import React from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
-import InputBase from '@material-ui/core/InputBase'
-import { createStyles, fade, Theme, makeStyles } from '@material-ui/core/styles'
-import SearchIcon from '@material-ui/icons/Search'
-
+import Typography from '@material-ui/core/Typography'
+import Burgher from './Sidebar'
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        backgroundNav: { backgroundColor: 'black' },
         root: {
-            flexGrow: 1,
-        },
-        menuButton: {
-            marginRight: theme.spacing(2),
-        },
-
-        search: {
-            position: 'relative',
-            borderRadius: theme.shape.borderRadius,
-            backgroundColor: fade(theme.palette.common.white, 0.15),
-            '&:hover': {
-                backgroundColor: fade(theme.palette.common.white, 0.25),
-            },
-            marginLeft: 0,
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                marginLeft: theme.spacing(1),
-                width: 'auto',
-            },
-        },
-        searchIcon: {
-            padding: theme.spacing(0, 2),
-            height: '100%',
-            position: 'absolute',
-            pointerEvents: 'none',
             display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
         },
-        inputRoot: {
-            color: 'inherit',
+        navColor: {
+            backgroundColor: 'black',
         },
-        inputInput: {
-            padding: theme.spacing(1, 1, 1, 0),
-            // vertical padding + font size from searchIcon
-            paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-            transition: theme.transitions.create('width'),
-            width: '100%',
-            [theme.breakpoints.up('sm')]: {
-                width: '0.001ch',
-                '&:focus': {
-                    width: '15ch',
-                },
-            },
+        titleColor: {
+            color: 'white',
         },
     })
 )
 
-export default function SearchAppBar() {
+export default function PersistentDrawerLeft() {
     const classes = useStyles()
 
     return (
         <div className={classes.root}>
-            <AppBar position="static" className={classes.backgroundNav}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.navColor}>
                 <Toolbar>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{ 'aria-label': 'search' }}
-                        />
-                    </div>
+                    <Burgher />
                 </Toolbar>
             </AppBar>
         </div>
