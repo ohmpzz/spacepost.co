b6cdd239e6a2c047404ebb5dd181876fd8ed6fd8
Ohm Piromrak <ohmpzzz@gmail.com>
2020-12-28 11:38:39 +0700

    update: remove un-used code

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,42 +1,12 @@
 import React from 'react'
 import { typography } from 'src/utils/typography'
-import {
-    createMuiTheme,
-    ThemeProvider,
-    SimplePaletteColorOptions,
-} from '@material-ui/core/styles'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import Helmet from 'react-helmet'
 import Router from 'src/pages/Router'
 
 typography.injectStyles()
 
-// const primary: SimplePaletteColorOptions = {
-//     light: '#',
-//     main: '#',
-//     dark: '#',
-//     contrastText: '#',
-// }
-
-// const secondary: SimplePaletteColorOptions = {
-//     light: '#',
-//     main: '#',
-//     dark: '#',
-//     contrastText: '#',
-// }
-
-// const error: SimplePaletteColorOptions = {
-//     light: '#',
-//     main: '#',
-//     dark: '#',
-//     contrastText: '#',
-// }
-
 const theme = createMuiTheme({
-    // palette: {
-    //     primary,
-    //     secondary,
-    //     error,
-    // },
     typography: {
         fontFamily: ['Libre Franklin'].join(''),
     },
